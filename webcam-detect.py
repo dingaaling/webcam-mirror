9c7bee65a36fa87c5869292928ee65fc3396a542
Jennifer Ding <jending12@gmail.com>
2020-01-11 10:45:05 -0500

    updated presets for two modes

--- webcam-detect.py
+++ webcam-detect.py
@@ -1,4 +1,4 @@
-import cv2, sys, requests
+import cv2, sys
 
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -16,13 +16,14 @@ ageModel = "age_net.caffemodel"
 genderProto = "gender_deploy.prototxt"
 genderModel = "gender_net.caffemodel"
 
+showEdge = 1
 while (video_capture.isOpened()):
 
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    k = cv2.waitKey(1)
+    k = cv2.waitKeyEx(1)
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.2,
@@ -56,25 +57,35 @@ while (video_capture.isOpened()):
         grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         edge = cv2.Canny(grayscale, 75, 125)
 
-        cv2.rectangle(edge, (x, y), (x+w, y+h), (255, 64, 64), 2)
 
+        if showEdge:
 
-        cv2.putText(edge, "Location: New York (40.75536, -73.96895)",(x+w+30, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
-        cv2.putText(edge, "Friend Peer Group: Starting Adult Life",(x+w+30, int(y + h*0.25)),cv2.FONT_HERSHEY_SIMPLEX, 0.75,(255,64,64),2, cv2.LINE_AA)
-        cv2.putText(edge, "Ad Interests: Espresso machine",(x+w+30, int(y + h*0.5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
-        cv2.putText(edge, "Clicked Ads: PubPass",(x+w+30, int(y + h*0.75)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
+            cv2.rectangle(edge, (x, y), (x+w, y+h), (255, 64, 64), 2)
+            cv2.putText(edge, "Location: 11201",(x+w+30, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
+            cv2.putText(edge, "Most Likely 311 Complaint: Sidewalk",(x+w+30, int(y + h*0.25)),cv2.FONT_HERSHEY_SIMPLEX, 0.75,(255,64,64),2, cv2.LINE_AA)
+            cv2.putText(edge, "Voter Status: Not Registered in New York",(x+w+30, int(y + h*0.5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
+            cv2.putText(edge, "Tax Bracket: ?",(x+w+30, int(y + h*0.75)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
 
-        # cv2.putText(edge, "Gender Prediction:" + str(gender),(x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,155,255),2, cv2.LINE_AA)
-        # cv2.putText(edge, "Age Estimation:" + str(age),(x, int(y + h*0.5)),cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,155,255),2, cv2.LINE_AA)
+            # Display the resulting frame
+            cv2.imshow('FaceDetection', edge)
 
-        # Display the resulting frame
-        cv2.imshow('FaceDetection', edge)
+        else:
+            cv2.rectangle(grayscale, (x, y), (x+w, y+h), (255, 64, 64), 2)
+            cv2.putText(grayscale, "Gender Prediction:" + str(gender),(x-w, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,155,255),2, cv2.LINE_AA)
+            cv2.putText(grayscale, "Age Estimation:" + str(age),(x-w, int(y + h*0.5)),cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,155,255),2, cv2.LINE_AA)
+
+            cv2.putText(grayscale, "Friend Peer Group: Starting Adult Life",(x+w+30, int(y + h*0.25)),cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,155,255),2, cv2.LINE_AA)
+            cv2.putText(grayscale, "Ad Interests: Espresso machine",(x+w+30, int(y + h*0.5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,155,255),2, cv2.LINE_AA)
+            cv2.putText(grayscale, "Clicked Ads: PubPass",(x+w+30, int(y + h*0.75)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,155,255),2, cv2.LINE_AA)
+
+            # Display the resulting frame
+            cv2.imshow('FaceDetection', grayscale)
 
     #ESC Pressed
-    if k%256 == 27:
+    if k== 27:
         break
     #SPACE pressed
-    elif k%256 == 32:
+    elif k== 32:
         img_name = "facedetect_webcam_{}.png".format(img_counter)
         cv2.imwrite(img_name, edge)
         print("{} written!".format(img_name))
