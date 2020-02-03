cd4837c3ccb3b4133dac03d7abcceeee28c0b03d
Jennifer Ding <jending12@gmail.com>
2020-02-03 17:24:46 -0500

    new styling

--- webcam-detect.py
+++ webcam-detect.py
@@ -10,7 +10,7 @@ padding = 20
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 genderList = ['Male', 'Female']
-ageList = ['(0 - 2)', '(4 - 6)', '(8 - 12)', '(15 - 20)', '(25 - 32)', '(38 - 43)', '(48 - 53)', '(60 - 100)']
+ageList = ['(4-6)', '(8 - 12)', '(15 - 20)', '(25 - 32)', '(25 - 32)', '(38 - 43)', '(48 - 53)', '(60 - 100)']
 
 ageProto = "age_deploy.prototxt"
 ageModel = "age_net.caffemodel"
@@ -72,16 +72,19 @@ def getFacebookData():
 
     peerGroupDisplay = peerGroup
     locationDisplay = random.sample(location, 1)[0]
-    adInterestDisplay = random.sample(adInterests, 1)[0]
+    adInterestDisplay = random.sample(adInterests, 3)
     adInteractDisplay = random.sample(adInteracts, 1)[0]
 
     return peerGroupDisplay, locationDisplay, adInterestDisplay, adInteractDisplay
 
 
-showEdge = 0
+showEdge = 1
 peerGroupDisplay, locationDisplay, adInterestDisplay, adInteractDisplay  = getFacebookData()
 ts = int(locationDisplay['creation_timestamp'])
 color=np.random.rand(3,)*255
+
+face_counter = 0
+gender_list, age_list = [], []
 
 while (video_capture.isOpened()):
 
@@ -93,74 +96,84 @@ while (video_capture.isOpened()):
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.2,
-        minNeighbors=7,
+        minNeighbors=9,
         minSize=(30, 30)
     )
 
-    # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
 
         face = frame[y:y+h, x:x+w]
 
-        ageNet = cv2.dnn.readNet(ageModel, ageProto)
-        genderNet = cv2.dnn.readNet(genderModel, genderProto)
+        if face_counter < 50:
+            face_counter+=1
+            ageNet = cv2.dnn.readNet(ageModel, ageProto)
+            genderNet = cv2.dnn.readNet(genderModel, genderProto)
 
-        blob = cv2.dnn.blobFromImage(face, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            blob = cv2.dnn.blobFromImage(face, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
 
-        genderNet.setInput(blob)
-        genderPreds = genderNet.forward()
-        gender = genderList[genderPreds[0].argmax()]
-        # print("Gender Output : {}".format(genderPreds))
-        # print("Gender : {}".format(gender))
+            genderNet.setInput(blob)
+            genderPreds = genderNet.forward()
+            # if genderPreds[0][0] < 0.7:
+            #     genderPreds[0][0] = 0
+            gender = genderList[genderPreds[0].argmax()]
+            gender_list.append(gender)
 
-        ageNet.setInput(blob)
-        agePreds = ageNet.forward()
-        age = ageList[agePreds[0].argmax()]
-        # print("Age Output : {}".format(agePreds))
-        # print("Age : {}".format(age))
+            ageNet.setInput(blob)
+            agePreds = ageNet.forward()
+            age = ageList[agePreds[0].argmax()]
+            age_list.append(age)
+
+        else:
+            gender = max(set(gender_list))
+            age = max(set(age_list))
 
 
         im_blur = cv2.GaussianBlur(frame,(7,7),0)
-
         grayscale = cv2.cvtColor(im_blur, cv2.COLOR_BGR2GRAY)
-        # edge = cv2.Canny(grayscale, 75, 125)
-        edge = cv2.Laplacian(grayscale,cv2.CV_64F)
-
-        grayscale_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-        corners = cv2.goodFeaturesToTrack(grayscale_face, 100, 0.01, 10)
-        corners = np.int0(corners)
-
 
         if showEdge:
 
-            cv2.rectangle(edge, (x, y), (x+w, y+h), (255, 64, 64), 2)
+            edge = cv2.Canny(grayscale,50, 100)
+            final_output_edge = cv2.dilate(edge,None)
 
-            cv2.putText(edge, "Location: 11201",(x+w+30, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
-            cv2.putText(edge, "Most Likely 311 Complaint: Noise - Residential",(x+w+30, int(y + h*0.25)),cv2.FONT_HERSHEY_SIMPLEX, 0.75,(255,64,64),2, cv2.LINE_AA)
-            cv2.putText(edge, "Voter Status: Not Registered in New York",(x+w+30, int(y + h*0.5)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
-            cv2.putText(edge, "Tax Bracket: ?",(x+w+30, int(y + h*0.75)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,64,64),2, cv2.LINE_AA)
+
+            cv2.rectangle(final_output_edge, (x, y), (x+w, y+h), (255, 255, 255), 4)
+            cv2.putText(final_output_edge, "Location: 11201",(x, y-50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255),5, cv2.LINE_AA)
+            cv2.putText(final_output_edge, "Voter Status: Not Registered in New York",(x-w+35, int(y + h*0.2)), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255, 255, 255),5, cv2.LINE_AA)
+            cv2.putText(final_output_edge, "Tax Bracket: 75 Percentile",(int(x-w*0.5), int(y + h*1.15)), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255, 255, 255),5, cv2.LINE_AA)
+            cv2.putText(final_output_edge, "Most Likely 311 Complaint: Noise - Residential",(x-w, int(y+h*1.4)),cv2.FONT_HERSHEY_SIMPLEX, 1.25,(255, 255, 255),5, cv2.LINE_AA)
+
 
             # Display the resulting frame
-            cv2.imshow('FaceDetection', edge)
+            cv2.imshow('FaceDetection', final_output_edge)
 
         else:
 
+            grayscale_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            corners = cv2.goodFeaturesToTrack(grayscale_face, 70, 0.005, 10)
+            corners = np.int0(corners)
+            # Pixelation: Get input size
+            im_width, im_height, _ = face.shape
+            temp = cv2.resize(face, (20, 20), interpolation=cv2.INTER_LINEAR)
+            final_face = cv2.resize(temp, (im_height, im_width), interpolation=cv2.INTER_NEAREST)
+            final_output = frame
+            final_output[y:y+h, x:x+w] = final_face
+
             for corner in corners:
                 cx,cy = corner.ravel()
-                cv2.circle(im_blur,(cx+x,cy+y),3,color,-1)
+                cv2.circle(final_output,(cx+x,cy+y),3,color,-1)
 
-            cv2.rectangle(im_blur, (x, y), (x+w, y+h), color, 2)
-            cv2.putText(im_blur, "Gender Prediction: " + str(gender),(int(x-w*1.2), int(y + h*0.25)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color,2, cv2.LINE_AA)
-            cv2.putText(im_blur, "Age Estimation: " + str(age),(int(x-w*1.2), int(y + h*0.5)),cv2.FONT_HERSHEY_SIMPLEX, 0.7,color,2, cv2.LINE_AA)
+            cv2.rectangle(final_output, (x, y), (x+w, y+h), color, 2)
+            cv2.putText(final_output, "Gender Prediction: " + str(gender),(10, 700), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color,2, cv2.LINE_AA)
+            cv2.putText(final_output, "Age Estimation: " + str(age),(450, 700),cv2.FONT_HERSHEY_SIMPLEX, 0.6,color,2, cv2.LINE_AA)
+            cv2.putText(final_output, "Friend Peer Group: " + peerGroupDisplay,(900, 700),cv2.FONT_HERSHEY_SIMPLEX, 0.6,color,2, cv2.LINE_AA)
 
-            cv2.putText(im_blur, "Friend Peer Group: " + peerGroupDisplay,(x+w+30, int(y + h*0.25)),cv2.FONT_HERSHEY_SIMPLEX, 0.7,color,2, cv2.LINE_AA)
-            cv2.putText(im_blur, "Ad Interest: " + adInterestDisplay,(x+w+30, int(y + h*0.5)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color,2, cv2.LINE_AA)
-            cv2.putText(im_blur, adInteractDisplay['action'] + ": " + adInteractDisplay['title'],(x+w+30, int(y + h*0.75)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color,2, cv2.LINE_AA)
-            cv2.putText(im_blur, "Detected location: " + locationDisplay['name'] + ' on ' + datetime.utcfromtimestamp(ts).strftime('%m/%d/%Y'), (x+w+30, int(y + h*0.95)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, color,2, cv2.LINE_AA)
-
+            cv2.putText(final_output, adInterestDisplay[0],(x, int(y - h*0.25)), cv2.FONT_HERSHEY_TRIPLEX, 1.5, color,4, cv2.LINE_AA)
+            cv2.putText(final_output, adInterestDisplay[1],(int(x-w*1.05), int(y + h*0.5)), cv2.FONT_HERSHEY_TRIPLEX, 1.5, color,4, cv2.LINE_AA)
+            cv2.putText(final_output, adInterestDisplay[2],(int(x+w), int(y + h*0.75)), cv2.FONT_HERSHEY_TRIPLEX, 1.5, color,4, cv2.LINE_AA)
 
             # Display the resulting frame
-            cv2.imshow('FaceDetection', im_blur)
+            cv2.imshow('FaceDetection', final_output)
 
     #ESC Pressed
     if k== 27:
@@ -177,12 +190,15 @@ while (video_capture.isOpened()):
             peerGroupDisplay, locationDisplay, adInterestDisplay, adInteractDisplay  = getFacebookData()
             ts = int(locationDisplay['creation_timestamp'])
             color=np.random.rand(3,)*255
-            print(color)
 
     #s pressed
     elif k== 115:
         img_name = "facedetect_webcam_{}.png".format(img_counter)
-        cv2.imwrite(img_name, edge)
+        if showEdge:
+            cv2.imwrite(img_name, final_output_edge)
+        else:
+            cv2.imwrite(img_name, final_output)
+
         print("{} written!".format(img_name))
         img_counter += 1
 
